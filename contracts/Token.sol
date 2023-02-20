4b8188a25c3a1cff1e64e80ef3a8b2195a58bfd9
craig-iam-smith <craig.iam.smith@gmail.com>
2023-02-20 16:03:57 -0700

    move to Token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,15 +5,23 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
+contract ERC1155plus is ERC1155, Ownable, Pausable, ERC1155Burnable {
+
+    // Mapping from owner to array of token IDs owned 
+    mapping(address => uint256[]) private _ownedTokens;
+    // Mapping from token ID to array of owners
+    mapping(uint256 => address[]) private _tokenOwners;
+    // Mapping from token ID to total tokens minted
+    mapping(uint256 => uint256) private _totalTokens;
+
     constructor() ERC1155("") {}
-
-
+/*
     function supportsInterface(bytes4 interfaceId) external view returns (bool) {
         // How should the check here be like?
     }
-    
+*/
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
@@ -45,14 +53,52 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
         whenNotPaused
         override
     {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+        uint256 idsLength = ids.length;
+        uint256 amountsLength =amounts.length;
+        require (idsLength > 0);
+        require (idsLength == amountsLength);
+        // if the from address is zero
+        if (from == address(0))
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);   
     }
 
     function payAllHolders(uint256 id, uint256 amount, address paymentTokenContract) public onlyOwner
     {
+        ERC20 payToken = ERC20(paymentTokenContract);
         uint256 i;
-        for (i=0;i<_balances[id].length;i++) {
-            
+        address to;
+        address[] memory payees = _tokenOwners[id];
+        uint256 end = payees.length;
+        require (paymentTokenContract != address(0));
+//        require (paymentTokenContract.balanceOf[msg.sender] > amount, "Must have enough of payment token");
+        uint256 percentage;
+        uint256 total=0;
+        uint256 ownership;
+        uint256 share;
+        for (i=0;i<end;i++) {
+            to = payees[i];
+            ownership = balanceOf(to, id);
+            share = ((amount * ownership) / _totalTokens[id]);
+            payToken.transferFrom(msg.sender, to, share);
+            total += share;
         }
+        require (total <= _totalTokens[id]);
     }
+
+    function getTotalTokens(uint256 id) public view returns (uint256) {
+        return _totalTokens[id];
+    }
+    // @dev 
+    // @params owner 
+    // @return returns array of token ids owned by this address
+    function getTokensOwned(address owner) public view returns (uint256[] memory) {
+        return _ownedTokens[owner];
+    }
+    // @dev
+    // @params tokenid
+    // @return returns array of owner addresses of this tokenid
+    function getOwnersOfToken(uint256 tokenId) public view returns (address[] memory) {
+        return _tokenOwners[tokenId];
+    }
+
 }
